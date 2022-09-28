a18a9eaad25b0adcfc018fdc1c6d54f945b21da7
Jose Arreola <65381222+JoseArreolaPrado@users.noreply.github.com>
2022-09-28 18:40:52 -0500

    Animations started

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_flutter/src/pages/animations_page.dart';
 import 'package:ui_flutter/src/pages/headers_pages.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Design App',
-      home: HeadersPage(),
+      home: AnimationsPage(),
     );
   }
 }
---
+++ lib/src/pages/animations_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AnimationsPage extends StatelessWidget {
+  const AnimationsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedSquare(),
+      ),
+    );
+  }
+}
+
+class AnimatedSquare extends StatefulWidget {
+  const AnimatedSquare({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AnimatedSquare> createState() => _AnimatedSquareState();
+}
+
+class _AnimatedSquareState extends State<AnimatedSquare> {
+  @override
+  Widget build(BuildContext context) {
+    return _Rectangle();
+  }
+}
+
+class _Rectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(color: Colors.blue),
+    );
+  }
+}
