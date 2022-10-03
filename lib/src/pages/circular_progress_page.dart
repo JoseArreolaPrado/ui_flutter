288367b1321ce15b528f24c37ec55175c3e92cf0
Jose Arreola <65381222+JoseArreolaPrado@users.noreply.github.com>
2022-10-03 17:27:56 -0500

    First animation for circular progres indicator

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -9,8 +10,34 @@ class CirculasProgresBar extends StatefulWidget {
   State<CirculasProgresBar> createState() => _CirculasProgresBarState();
 }
 
-class _CirculasProgresBarState extends State<CirculasProgresBar> {
-  double percentage = 10;
+class _CirculasProgresBarState extends State<CirculasProgresBar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  double percentage = 10.0;
+  double newPercentage = 0.0;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    );
+
+    controller.addListener(() {
+      // print('Contoller value: ${controller.value}');
+      setState(() {
+        percentage = lerpDouble(percentage, newPercentage, controller.value)!;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +45,13 @@ class _CirculasProgresBarState extends State<CirculasProgresBar> {
         backgroundColor: Colors.teal,
         child: const Icon(Icons.refresh),
         onPressed: () {
-          percentage += 10;
-          if (percentage > 100) {
+          percentage = newPercentage;
+          newPercentage += 10;
+          if (newPercentage > 100) {
+            newPercentage = 0;
             percentage = 0;
           }
+          controller.forward(from: 0.0);
           setState(() {});
         },
       ),
