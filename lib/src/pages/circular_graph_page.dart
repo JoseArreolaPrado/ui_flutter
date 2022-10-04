c00ea1888380910937c4dbf46ef0db83cea54f3a
Jose Arreola <65381222+JoseArreolaPrado@users.noreply.github.com>
2022-10-04 09:18:45 -0500

    Widget finished

--- lib/src/pages/circular_graph_page.dart
+++ lib/src/pages/circular_graph_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_flutter/main.dart';
 import 'package:ui_flutter/src/widgets/radial_progress.dart';
 
 class CircularGraphPage extends StatefulWidget {
@@ -11,27 +12,58 @@ class _CircularGraphPageState extends State<CircularGraphPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.teal,
-        child: const Icon(Icons.refresh),
-        onPressed: () {
-          setState(() {
-            percentage += 10.0;
-            if (percentage > 100) {
-              percentage = 0;
-            }
-          });
-        },
-      ),
-      body: Center(
-          child: SizedBox(
-        height: 300,
-        width: 300,
-        child: RadialProgress(
-          percentage: percentage,
-          arcColor: Colors.orange,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.teal,
+          child: const Icon(Icons.refresh),
+          onPressed: () {
+            setState(() {
+              percentage += 10.0;
+              if (percentage > 100) {
+                percentage = 0;
+              }
+            });
+          },
         ),
-      )),
-    );
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CustomRadialProgress(
+                    percentage: percentage,
+                    color: Colors.black,
+                  ),
+                  CustomRadialProgress(
+                    percentage: percentage,
+                    color: Colors.teal,
+                  ),
+                ],
+              )
+            ]));
+  }
+}
+
+class CustomRadialProgress extends StatelessWidget {
+  final Color color;
+  const CustomRadialProgress({
+    Key? key,
+    required this.percentage,
+    this.color = Colors.blue,
+  }) : super(key: key);
+
+  final double percentage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: SizedBox(
+      height: 150,
+      width: 150,
+      child: RadialProgress(
+        percentage: percentage,
+        arcColor: color,
+      ),
+    ));
   }
 }
