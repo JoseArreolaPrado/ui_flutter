da016b9126d811de4deb9925086eafe67ed8bf50
Jose Arreola <65381222+JoseArreolaPrado@users.noreply.github.com>
2022-10-10 17:58:45 -0500

    Thick button UI set

--- lib/src/widgets/thick_button.dart
+++ lib/src/widgets/thick_button.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ThickButton extends StatelessWidget {
   const ThickButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return _ThickButtonBackground();
+    return Stack(children: [
+      _ThickButtonBackground(),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          SizedBox(height: 140, width: 40),
+          FaIcon(FontAwesomeIcons.carCrash, color: Colors.white, size: 40),
+          SizedBox(width: 20),
+          Expanded(
+            child: Text(
+              'Motor accident',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          FaIcon(FontAwesomeIcons.chevronRight, color: Colors.white),
+          SizedBox(width: 40),
+        ],
+      )
+    ]);
   }
 }
 
@@ -17,6 +39,21 @@ class _ThickButtonBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(35),
+        child: Stack(
+          children: [
+            Positioned(
+                right: -20,
+                top: -20,
+                child: FaIcon(
+                  FontAwesomeIcons.carCrash,
+                  size: 150,
+                  color: Colors.white.withOpacity(0.2),
+                ))
+          ],
+        ),
+      ),
       margin: const EdgeInsets.all(20),
       decoration: BoxDecoration(
           boxShadow: <BoxShadow>[
