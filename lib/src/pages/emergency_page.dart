6ae6a48c2dc36faa4fb82bab7aecd354efe0d6a9
Jose Arreola <65381222+JoseArreolaPrado@users.noreply.github.com>
2022-10-11 10:19:26 -0500

    Added list of buttons

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -4,20 +4,103 @@ import 'package:ui_flutter/src/widgets/thick_button.dart';
 
 import '../widgets/icon_header_widget.dart';
 
+class ItemBoton {
+  final IconData icon;
+  final String text;
+  final Color color1;
+  final Color color2;
+
+  ItemBoton(
+    this.icon,
+    this.text,
+    this.color1,
+    this.color2,
+  );
+}
+
 class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final items = <ItemBoton>[
+      ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5),
+          Color(0xff906EF5)),
+      ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2),
+          Color(0xff536CF6)),
+      ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+      ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5),
+          Color(0xff906EF5)),
+      ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2),
+          Color(0xff536CF6)),
+      ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+      ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5),
+          Color(0xff906EF5)),
+      ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2),
+          Color(0xff536CF6)),
+      ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+    ];
+
+    Iterable itemMap = items
+        .map((item) => ThickButton(
+              text: item.text,
+              onPress: () {
+                print('Something');
+              },
+              icon: item.icon,
+              color1: item.color1,
+              color2: item.color2,
+            ))
+        .toList();
+
     return Scaffold(
-      body: Center(
-        child: ThickButton(
-          icon: FontAwesomeIcons.carCrash,
-          onPress: () {
-            print('Tap');
-          },
-          color1: const Color(0xff6989f5),
-          color2: const Color(0xfff906ef5),
-          text: 'Motor Accident',
-        ),
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 200),
+            child: ListView(
+              children: <Widget>[
+                const SizedBox(height: 80),
+                ...itemMap,
+              ],
+            ),
+          ),
+          const IconHeader(
+            icon: FontAwesomeIcons.plus,
+            title: 'Medical support',
+            subtitle: 'Requrested',
+            color1: Color(0xff526bf6),
+            color2: Color(0xff67acf2),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ThickButtonTemp extends StatelessWidget {
+  const ThickButtonTemp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ThickButton(
+        icon: FontAwesomeIcons.carCrash,
+        onPress: () {
+          print('Tap');
+        },
+        color1: const Color(0xff6989f5),
+        color2: const Color(0xfff906ef5),
+        text: 'Motor Accident',
       ),
     );
   }
